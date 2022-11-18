2fef9e1566f60f32718f872f38bc9c01af21cfec
yarlei.marques <yarlei.marques@sspds.ce.gov.br>
2022-11-18 14:02:56 -0300

    receba

--- pages/homePage/components/drawer/new_oficina/new_oficina.js
+++ pages/homePage/components/drawer/new_oficina/new_oficina.js
@@ -1,15 +1,21 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Button, StyleSheet, Text, TextInput, TouchableHighlight, View } from 'react-native'
 import React, { useState } from 'react'
 import KeyboardAvoidingView from 'react-native/Libraries/Components/Keyboard/KeyboardAvoidingView'
 
 const NewOficina = () => {
     const [endereco, setEndereco] = useState("")
     const [servicos, setServicos] = useState([])
+
+
+     function adicionarOficina(){
+        console.log("vai da tudo certo boy");
+    }
+
   return (
     <View style={styles.NewOficinaContainer}>
-        <KeyboardAvoidingView 
-        style={styles.enderecoViewContainer}>
-            <View>
+        <KeyboardAvoidingView>
+            <View
+            style={styles.enderecoViewContainer}>
                 <TextInput 
                 multiline={true}
                 placeholder='Endereco'
@@ -19,14 +25,21 @@ const NewOficina = () => {
                 />
             </View>
             <View style={styles.bottomContainer}>
-                <View>
-                    <Text>Lista de services</Text>
+                <View style={styles.adicionarService}>
+                    <TextInput 
+                    placeholder='Servico'
+                    style={styles.servicoTextInput}
+                    />
+                    <TextInput 
+                    placeholder='Preco'
+                    style={styles.enderecoViewContainer}
+                    />
                 </View>
                 <View>
-                    <Button 
-                    title='Adicionar'
-                    style={styles.buttonAdd}
-                    />
+                    <TouchableHighlight 
+                    onPress={adicionarOficina()}>
+                        <Text style={styles.buttonAdd}>Adicionar</Text>
+                    </TouchableHighlight>
                 </View>
             </View>
             
@@ -41,18 +54,33 @@ export default NewOficina
 const styles = StyleSheet.create({
     NewOficinaContainer:{
         padding: 10,
-        width:"100%",
-        height:"100%"
+        width: "100%",
+        flex: 1
     },
     enderecoViewContainer:{
-        flex: 0.04,
-        height: 50,
+        height: 55,
         padding:10,
         borderColor: "black",
         borderWidth: 1,
     },
     bottomContainer:{
-        paddingTop:1,
-        flexDirection:"row"
+        paddingTop:10,
+        justifyContent: 'space-around',
+        alignItems:'center' 
+        
     },
+    adicionarEretirar:{
+        padding:10,
+        fontSize: 25
+    },
+    adicionarService:{
+        flexDirection: 'row',
+        width: "100%"
+    },
+    buttonAdd:{
+        padding: 10,
+        backgroundColor: "grey",
+        borderRadius: 10,
+        color: "white"
+    }
 })
